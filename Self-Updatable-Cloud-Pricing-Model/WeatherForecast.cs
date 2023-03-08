20df7ba0c980ec75e6fa15365c406523bec6a495
Albert1708 <albert.rozario3@gmail.com>
2023-03-08 20:51:02 +0100

    Created VS project

---
+++ Self-Updatable-Cloud-Pricing-Model/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Self_Updatable_Cloud_Pricing_Model
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
